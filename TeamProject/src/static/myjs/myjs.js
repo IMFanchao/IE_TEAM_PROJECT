9f0725b8d8e676fb903569feedef70a8acc30250
Finlay <finlay@dyn-49-127-149-157.its.monash.edu.au>
2019-04-28 23:52:07 +1000

    activity_level health_tips homepage

--- TeamProject/src/static/myjs/myjs.js
+++ TeamProject/src/static/myjs/myjs.js
@@ -1,6 +1,6 @@
 //@ Finlay JS
 $(document).ready(function(){
-	var num_ac = 1
+	var num_ac = 1;
 	$("#activity_list").on("click",".add_ac",function(){
 		num_ac++
 		// Determine if the user enters a valid value
@@ -14,7 +14,6 @@ $(document).ready(function(){
 		else{
 			alert("Should input value");
 		}
-
 	})
 
 	// Can't delete the first one
@@ -64,27 +63,13 @@ $(document).ready(function(){
 		for (var i = finalAcList.length - 1; i >= 0; i--) {
 			var typeofac = finalAcList[i][0];
 			var num_hr = finalAcList[i][1];
-			switch(typeofac){
-				case 0:
-					break;
-				case 1:
-					ac_value = ac_value + (3*num_hr);
-					break;
-				case 2:
-					ac_value = ac_value + (2*num_hr);
-					break;
-				case 3:
-					ac_value = ac_value + (2.5*num_hr);
-					break;
+        ac_value = ac_value + (typeofac*num_hr*60)
 			}
-
+      return ac_value;
 		}
-		return ac_value;
-
-	}
-
 
 	$("#check").click(function(){
+    var ac_level = 0;
 		var age_group = parseInt($("#selectAge").val());
 
 		age_group = parseInt(age_group);
@@ -98,11 +83,85 @@ $(document).ready(function(){
 
 		var finalAcList = combineList(hrList, acList);
 		var score = calculateAcLevel(age_group, finalAcList);
-		$("p").text(score);
+    if (score<= 600) {
+      ac_level = 1;
+			$('#printLevel').text("Lack of exercise");
+    }
+    else if ((score>600) && (score<= 4000)) {
+      ac_level = 2;
+			$('#printLevel').text("Still need more exercise");
+			$("#DI").text("14%");
+			$("#CC").text("10%");
+			$("#IHD").text("16%");
+			$("#IS").text("16%");
+    }
+    else if ((score>4000) && (score<=8000)) {
+      ac_level = 3;
+			$('#printLevel').text("Well done, keep it");
+			$("#DI").text("25%");
+			$("#CC").text("17%");
+			$("#IHD").text("23%");
+			$("#IS").text("19%");
+    }
+    else if ((score>8000) && (score<=10000)){
+      ac_level = 4;
+			$('#printLevel').text("sufficient exercise");
+			$("#DI").text("28%");
+			$("#CC").text("21%");
+			$("#IHD").text("25%");
+			$("#IS").text("26%");
+    }
+    else {
+      ac_level = 5;
+			$('#printLevel').text("Excessive exercise may hurt the body");
+			$("#DI").text("28%");
+			$("#CC").text("21%");
+			$("#IHD").text("25%");
+			$("#IS").text("26%");
+    }
+
+		$('.chart').data('easyPieChart').update((score/10000)*100);
+		var printValue = 0;
+		if (((score/10000)*100)>100) {
+			 printValue = 100;
+		}
+		else {
+			 printValue = (score/10000)*100;
+		}
+
+		$('#precentage').text(printValue.toFixed(2));
 	})
+	//health_tips below
 
+	function randomTips(maxLength) {
 
+		var tips_index = parseInt(Math.random()*maxLength);
+		return tips[tips_index];
+	}
 
-
+	$("#Home_page_health_tips").text(function(){
+		var tips = ["Sugary drinks are the most fattening things you can put into your body.",
+								"Despite being high in fat, nuts are incredibly nutritious and healthy.",
+								"All the processed junk foods in the diet are the biggest reason the world is fatter and sicker than ever before.",
+								"Coffee has been unfairly demonized. The truth is that it's actually very healthy.",
+								"Pretty much everyone agrees that fish is healthy.",
+								"The importance of getting enough quality sleep can not be overstated.",
+								"The bacteria in your gut, collectively called the gut microbiota, are sometimes referred to as the forgotten organ.",
+								"Drinking enough water can have numerous benefits.",
+								"Meat can be a nutritious and healthy part of the diet. It is very high in protein, and contains various important nutrients.",
+								"Back in the day, most people got their vitamin D from the sun.",
+								"Vegetables and fruits are the default health foods, and for good reason.",
+								"Eating enough protein is incredibly important, and many experts believe that the recommended daily intake is too low.",
+								"Doing aerobic exercise (or cardio) is one of the best things you can do for your mental and physical health.",
+								"If you're a tobacco smoker, or abuse drugs, then diet and exercise are the least of your worries. Tackle those problems first.",
+								"Extra virgin olive oil is the healthiest fat on the planet.",
+								"Added sugar is the single worst ingredient in the modern diet.",
+								"Don't Eat a Lot of Refined Carbohydrates",
+								"The war on saturated fat was a mistake.",
+								"Lifting weights is one of the best things you can do to strengthen your body and improve your body composition.",
+								"Artificial trans fats are harmful, man-made fats that are strongly linked to inflammation and heart disease."];
+		var tips_index = parseInt(Math.random()*tips.length);
+		return tips[tips_index];
+	})
 })
 //@ Finlay JS
